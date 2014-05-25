1ac662b8792f8186ac1f39495ecd9cb228e6d58b
Matt Carroll <mattc@yelp.com>
2014-05-25 21:46:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+avr-uart
+========
+
+A library for using the UART for serial output on an AVR chip
+
+This code will probably have specific options for specific devices in stupid
+places because FI;SI.
---
+++ uart.c
@@ -0,0 +1,43 @@
+#include <avr/io.h>
+#include <stdio.h>
+
+#define F_CPU 16000000UL
+
+#define BAUD 9600
+
+#include <util/setbaud.h>
+#define THIS_UBRR (((F_CPU/16)/BAUD)-1)
+
+void uart_init(void){
+  //set baud split across two registers
+  //UBRR0H = (uint8_t)(THIS_UBRR>>8);
+  //UBRR0L = (uint8_t)THIS_UBRR;
+  UBRR0H = UBRRH_VALUE;
+  UBRR0L = UBRRL_VALUE;
+
+#if USE_2X
+  UCSR0A |= 1<<U2X0;
+#else
+  UCSR0A &= ~(1<<U2X0);
+#endif
+
+  //enable TX and RX
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+
+  //8 bit communication with one stop bit (011)
+  //using registers UCSZ20, UCSZ01, UCSZ00
+  UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+}
+
+void uart_putchar(char c, FILE *stream){
+  if (c == '\n'){
+    uart_putchar('\r', stream);
+  }
+  while(!(UCSR0A & (1<<UDRE0)));
+  UDR0 = c;
+}
+
+char uart_getchar(void){
+  while(!(UCSR0A & (1<<RXC0)));
+  return UDR0;
+}
